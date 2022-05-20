8b2671ee95b9266a0457a7a33b2f5233be02e8f5
Hsiang Wei Huang <93310655+hsiangwei0903@users.noreply.github.com>
2022-05-20 15:36:40 -0700

    Update mk_video.py

--- mk_video.py
+++ mk_video.py
@@ -1,8 +1,14 @@
+'''
+To use this, please install ffmpeg.
+
+https://ffmpeg.org/
+
+'''
 import glob
 import os
 
-imgs_path = glob.glob('/home/wei/Desktop/wyze/*')
-out_path = '/home/wei/Desktop/output'
+imgs_path = glob.glob('/home/wei/Desktop/wyze/*') # image sequences dir
+out_path = '/home/wei/Desktop/output' # output dir
 for img in imgs_path:
     name = img.replace('/home/wei/Desktop/wyze/','')
     cmd_str = 'ffmpeg -f image2 -i {} -c:v copy {}'.format(img+'/'+'%04d'+'.jpg',out_path+'/'+name+'.mp4')
